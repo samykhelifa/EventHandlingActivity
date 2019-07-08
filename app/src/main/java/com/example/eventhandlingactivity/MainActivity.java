f80b8888308bd6433ed631d5f188977be95acf83
samy <ryio69007@gmail.com>
2019-07-09 00:49:28 +0200

    initial project setup

--- app/src/main/java/com/example/eventhandlingactivity/MainActivity.java
+++ app/src/main/java/com/example/eventhandlingactivity/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.eventhandlingactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        relativeLayout = findViewById(R.id.relativeLayout);
+        Button button1= findViewById(R.id.button1);
+        Button button2= findViewById(R.id.button2);
     }
 }
